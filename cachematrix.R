e1af7b487684b079bf77d1ab8b2e972284b27e3e
redsketch <zaqr.jb@gmail.com>
2014-07-25 15:15:11 +0700

    Add comments

--- cachematrix.R
+++ cachematrix.R
@@ -6,16 +6,17 @@
 makeCacheMatrix <- function(x = matrix()) {
     matrix <- NULL
     
+    ## A set of closure functions
     set <- function(y, ...) {
         x <<- matrix(y, ...)
         matrix <<- NULL
     }
-    
     get <- function() x
     setinverse <- function(inverse) matrix <<- inverse
     getinverse <- function() matrix
     testinverse <- function() round(matrix %*%x)
-        
+    
+    ## Create a list of functions, so the closure function can be called outside
     list(set = set, get = get, 
          setinverse = setinverse,
          getinverse = getinverse,
@@ -28,12 +29,17 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
     ## Return a matrix that is the inverse of 'x'
+    
+    ## Assign getinverse to matrix
     matrix <- x$getinverse()
+    
+    ## If matrix does not null, get the inverse from cached and then pass/return it
     if(!is.null(matrix)) {
         message("getting cached data")
         return(matrix)
     }
     
+    ## Solve the inverse
     data <- x$get()
     matrix <- solve(data)
     x$setinverse(matrix)
